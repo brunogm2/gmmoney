601cf065740114e04ab560fd03012c142ae07cc9
Bruno Gonçalves <brunobgm002@gmail.com>
2022-05-28 15:54:08 -0300

    ✨ : implementing the context api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,15 +1,45 @@
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
+
+import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
+
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container, TransactionTypeContainer } from './styles';
 
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
  interface NewTransactionModalProps {
      isOpen: boolean;
      onRequestClose: () => void;
  }
  
  export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const { createTransaction } = useTransactions();
+
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [category, setCategory] = useState('');
+
+    const [type, setType] = useState('deposit'); 
+
+    async function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+
+        await createTransaction({
+            title,
+            amount,
+            category,
+            type,
+        })
+
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
+    }
+
     return (
         <Modal 
             isOpen={isOpen}
@@ -25,36 +55,48 @@ import { Container, TransactionTypeContainer } from './styles';
             <img src={closeImg} alt="Fechar Modal" />
         </button>
          
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
             <h2>Cadastrar transação</h2>
 
             <input 
                 placeholder="Título"
+                value={title}
+                onChange={event => setTitle(event.target.value)}
             />
 
             <input 
                 type="number" 
                 placeholder="Valor"
+                value={amount}
+                onChange={event => setAmount(Number(event.target.value))}
             />
 
             <TransactionTypeContainer>
-                <button
-                    type="button" 
+                <RadioBox
+                    type="button"
+                    onClick={() => { setType('deposit') }}
+                    isActive={type === 'deposit'}
+                    activeColor="green"
                 >
                     <img src={incomeImg} />
                     <span>Entrada</span>
-                </button>
+                </RadioBox>
 
-                <button
+                <RadioBox
                     type="button" 
+                    onClick={() => { setType('withdraw') }}
+                    isActive={type === 'withdraw'}
+                    activeColor="red"
                 >
                     <img src={outcomeImg} />
                     <span>Saída</span>
-                </button>
+                </RadioBox>
             </TransactionTypeContainer>
 
             <input 
                 placeholder="Categoria"
+                value={category}
+                onChange={event => setCategory(event.target.value)}
             />
 
             <button type="submit">
---
+++ src/hooks/useTransactions.tsx
@@ -0,0 +1,61 @@
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react';
+import { api } from '../services/api';
+
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+interface TrnsactionsProviderProps {
+    children: ReactNode;
+}
+
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+const TransactionContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
+
+export function TransactionsProvider({ children }: TrnsactionsProviderProps) {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    useEffect(() => {
+        api.get('/transactions')
+            .then(response => setTransactions(response.data.transactions))
+    }, []);
+
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('transactions', {
+            ...transactionInput,
+            createdAt: new Date()
+        });
+        
+        const transaction = response.data.transactions;
+        
+        setTransactions([
+            ...transactions,
+            transaction
+        ]);
+    }
+
+    return (
+        <TransactionContext.Provider value={{ transactions, createTransaction}}>
+            {children}
+        </TransactionContext.Provider>
+    );
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionContext);
+
+    return context;
+}
